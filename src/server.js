499666773411f32a16a8a6eee3372b002e1a520a
Sara Hrnciar <110135122+shrncr@users.noreply.github.com>
2025-09-22 10:48:26 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config({ path: "./.env" });
+//require("dotenv").config({ path: "./.env" });
 
 const { watson } = require('./watson');
 
